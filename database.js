6a5ec38d60a5922a1f22412aec78201eff030b49
Deepak <55078370+gitsdeepak@users.noreply.github.com>
2019-12-17 11:48:03 +0530

    Update database.js

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 var app = angular.module("myApp", []);
 app.controller("myCtrl", function($scope) {
     $scope.whole=[
-        {Name:"Do you want to visit Goole?",
+        {Name:"Do you want to visit Google?",
          URL:"https://www.google.co.in/",
         logo: "https://cdn.vox-cdn.com/thumbor/Pkmq1nm3skO0-j693JTMd7RL0Zk=/0x0:2012x1341/1200x800/filters:focal(0x0:2012x1341)/cdn.vox-cdn.com/uploads/chorus_image/image/47070706/google2.0.0.jpg"
         },
@@ -9,7 +9,7 @@ app.controller("myCtrl", function($scope) {
          URL:"https://www.facebook.com",
         logo: "https://cdn.worldvectorlogo.com/logos/facebook-1.svg"
         },
-        {Name:"Do you want to visit youTube?",
+        {Name:"Do you want to visit YouTube?",
          URL:"https://www.youtube.com/",
        logo: "https://www1-lw.xda-cdn.com/files/2017/08/After-12-Years-Google-Gives-YouTube-a-New-Logo.png"
         },
